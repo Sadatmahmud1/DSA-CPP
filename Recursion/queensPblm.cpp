41b502365c86a3f60b3d9a627c1cd8ab49954b91
Sadat Mahmud <sadatmahmud1@outlook.com>
2024-06-08 08:11:09 +0600

    bug fixed in queens pblm

--- Recursion/queensPblm.cpp
+++ Recursion/queensPblm.cpp
@@ -2,8 +2,10 @@
 
 using namespace std;
 
+// Function to check if a queen can be placed at position (x, y)
 bool isSafe(int **arr, int x, int y, int n)
 {
+    // Check the column
     for (int row = 0; row < x; row++)
     {
         if (arr[row][y] == 1)
@@ -12,6 +14,7 @@ bool isSafe(int **arr, int x, int y, int n)
         }
     }
 
+    // Check the upper-left diagonal
     int row = x;
     int col = y;
     while (row >= 0 && col >= 0)
@@ -23,10 +26,11 @@ bool isSafe(int **arr, int x, int y, int n)
         row--;
         col--;
     }
-        // Check the upper-right diagonal
+
+    // Check the upper-right diagonal
     row = x;
     col = y;
-    while (row >= 0 && col >= 0)
+    while (row >= 0 && col < n)
     {
         if (arr[row][col] == 1)
         {
@@ -39,6 +43,7 @@ bool isSafe(int **arr, int x, int y, int n)
     return true;
 }
 
+// Function to solve the N-Queens problem using backtracking
 bool nQueen(int **arr, int x, int n)
 {
     if (x >= n)
@@ -50,46 +55,60 @@ bool nQueen(int **arr, int x, int n)
     {
         if (isSafe(arr, x, col, n))
         {
-            arr[x][col]=1;
-        }
+            arr[x][col] = 1;
 
-        if (nQueen(arr,x+1,n))
-        {
-            return true;
+            // Proceed to place the next queen
+            if (nQueen(arr, x + 1, n))
+            {
+                return true;
+            }
+
+            // Backtrack if placing queen in current position doesn't lead to a solution
+            arr[x][col] = 0;
         }
-        arr[x][col]=0; //Backtracking 
     }
     return false;
 }
 
 int main()
 {
-
     int n;
-    cin>>n;
-    int** arr = new int*[n];
-    for(int i = 0; i < n; i++){
+    cin >> n;
+
+    // Dynamically allocate a 2D array
+    int **arr = new int*[n];
+    for (int i = 0; i < n; i++)
+    {
         arr[i] = new int[n];
         for (int j = 0; j < n; j++)
         {
-            arr[i][j] = 0 ;
+            arr[i][j] = 0;
         }
     }
 
-    if(nQueen(arr,0,n)){
-        for(int i = 0; i < n; i++){
-            for(int j=0; j<n;j++){
-                cout<<arr[i][j]<<" ";
-            }cout<<endl;
+    // Solve the N-Queens problem and print the solution
+    if (nQueen(arr, 0, n))
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << arr[i][j] << " ";
+            }
+            cout << endl;
+        }
     }
+    else
+    {
+        cout << "No solution exists" << endl;
     }
 
-    // Free alocated memory
-    for(int i = 0; i < n; i++){
+    // Free allocated memory
+    for (int i = 0; i < n; i++)
+    {
         delete[] arr[i];
-
-        delete[] arr;
     }
+    delete[] arr;
 
     return 0;
 }
