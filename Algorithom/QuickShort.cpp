efe135bc6dc2163c1206d9cf5bf3f97194669725
Sadat Mahmud <sadatmahmud1@outlook.com>
2024-06-22 01:53:28 +0600

    fixes quick sort

--- Algorithom/QuickShort.cpp
+++ Algorithom/QuickShort.cpp
@@ -2,39 +2,43 @@
 
 using namespace std;
 
-int  swap(int arr,int i,int j)
-{
+void swap(int arr[], int i, int j) {
     int temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
 }
 
-int partition(int arr[], int l, int h)
-{
+int partition(int arr[], int l, int h) {
     int pivot = arr[h];
     int i = l - 1;
-    for (int j = l; j < h; j++)
-    {
-        if (arr[j] < pivot)
-        {
+    for (int j = l; j < h; j++) {
+        if (arr[j] < pivot) {
             i++;
-            swap(arr,i,j);
+            swap(arr, i, j);
         }
     }
+    swap(arr, i + 1, h);
+    return i + 1;
 }
 
-void quickSort(int arr[], int l, int h)
-{
-    if (l < h)
-    {
+void quickSort(int arr[], int l, int h) {
+    if (l < h) {
         int p = partition(arr, l, h);
         quickSort(arr, l, p - 1);
-        quickSort(arr, h, p + 1);
+        quickSort(arr, p + 1, h);
     }
 }
 
-int main()
-{
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
