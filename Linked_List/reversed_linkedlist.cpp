40877772158735a6c428778f37460801dd41ac0a
Sadat Mahmud <sadatmahmud1@outlook.com>
2024-07-11 14:48:36 +0600

    added reversed linked list

---
+++ Linked_List/reversed_linkedlist.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+// through the temp pointer we are modifying original nodes next (linker)
+// but head will be alwys points the first node.
+// so thet we can access the first node and traverse it.
+
+void insertAtTail(node* &head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = n;
+}
+
+node* reversePtr(node* &head){
+    node* prev = NULL;
+    node* curr = head;
+    node* next;
+
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next  = prev;
+
+        prev = curr;
+        curr = next;
+
+
+
+    }
+      head = prev;  // Update the head to the new head
+    return head;  // Return the new head
+
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    display(head);
+
+   node* newHead = reversePtr(head);
+
+    display(newHead);
+
+    return 0;
+}
