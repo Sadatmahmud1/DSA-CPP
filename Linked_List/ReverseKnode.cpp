369b652b389e24b99ccaf6c51afbecd3b05f8287
Sadat Mahmud <sadatmahmud1@outlook.com>
2024-07-12 09:17:26 +0600

    added reverse k node and bunch of coments

--- Linked_List/ReverseKnode.cpp
+++ Linked_List/ReverseKnode.cpp
@@ -17,9 +17,9 @@ public:
 // but head will be alwys points the first node.
 // so thet we can access the first node and traverse it.
 
-void insertAtTail(node* &head, int val)
+void insertAtTail(node *&head, int val)
 {   // if head is null it meas there is no node so that initially created node will be the first
-// node and head will point to it. also it will return and rest of the code will never be exicutated
+    // node and head will point to it. also it will return and rest of the code will never be exicutated
     node *n = new node(val);
     if (head == NULL)
     {
@@ -35,29 +35,36 @@ void insertAtTail(node* &head, int val)
     temp->link = n;
 }
 
-node* reverseKnode(node* &head){
-    node* prev = NULL;
-    node* curr = head;
-    node* next;
-
-    while (curr != NULL)
+node *reverseKnode(node *&head, int k)
+{
+    node *prev = NULL;
+    node *curr = head;
+    node *next;
+    int count = 0;
+    // this loop runs k time instade of reversing full linked list
+    while (curr != NULL && count < k)
     {
         // We points thrue curr->link to next element
-        //it stores through curr->link address of next
+        // it stores through curr->link address of next
         next = curr->link;
-        //this line changing the address of curr's link address now it contain the address of prev
-        // curr->link == next
-        curr->link  = prev;
+        // this line changing the address of curr's link address now it contain the address of prev
+        //  curr->link == next
+        curr->link = prev;
 
         // now we are moving prev to curr and setting it as new prev for next itration
         prev = curr;
         // now wer are moving current to next and setting it as new prev for next itration
         curr = next;
-
+        count++;
     }
-      head = prev;  // Update the head to the new head
-    return head;  // Return the new head
 
+    if (next != NULL)
+    {
+        //this line is conecting first reversed pairs to second pairs head
+        // through previous heads head->link and putting the address of next
+        head->link = reverseKnode(next, k);
+    }
+    return prev;
 }
 
 void display(node *head)
@@ -78,9 +85,11 @@ int main()
     insertAtTail(head, 2);
     insertAtTail(head, 3);
     insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtTail(head, 6);
     display(head);
 
-   node* newHead = reverseKnode(head);
+    node *newHead = reverseKnode(head, 2);
 
     display(newHead);
 
